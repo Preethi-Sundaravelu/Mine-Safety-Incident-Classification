28baf20cd5673db4e3525244a46dddffe1fe19bc
KCSPreethi <preethi@sahaj.ai>
2023-07-19 07:58:03 +0530

    Modify app to include basic template

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from src.all_imports import *
 
 app = Flask(__name__)
+data = dict()
+
+# model = pickle.load(open('xgb_pkl.pkl', 'rb'))
 
 @app.route('/')
 def index():
@@ -8,18 +11,13 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    
+
     columns_file_obj = open('src\columns.txt', 'r')
     columns_list = columns_file_obj.read()
     data = {column: request.form.get(column) for column in columns_list}
     columns_file_obj.close()
 
-    encoded_data_for_model_input = label_encoder.fit_transform(list(data.values()))
-
-    prediction = gbnn_model.predict(encoded_data_for_model_input)
-    prediction_text = label_encoder.inverse_transform(prediction)
-    
-    return render_template('templates\index.html', prediction_text = prediction_text)
+#     prediction = model.predict(list(data.values()))
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
