4a9a7a65859b658866595177112d237e4c9be9f3
rakulan-srinivasan <vsriabilarag@live.com>
2023-07-19 06:42:13 +0530

    Add Backend Functions

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from src.all_imports import *
 
 app = Flask(__name__)
-data = dict()
-
-# model = pickle.load(open('xgb_pkl.pkl', 'rb'))
 
 @app.route('/')
 def index():
@@ -17,7 +14,12 @@ def predict():
     data = {column: request.form.get(column) for column in columns_list}
     columns_file_obj.close()
 
-#     prediction = model.predict(list(data.values()))
+    encoded_data_for_model_input = label_encoder.fit_transform(list(data.values()))
+
+    prediction = gbnn_model.predict(encoded_data_for_model_input)
+    prediction_text = label_encoder.inverse_transform(prediction)
+    
+    return render_template('templates\index.html', prediction_text = prediction_text)
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)
--- src/all_imports.py
+++ src/all_imports.py
@@ -3,3 +3,11 @@ from flask import Flask, render_template, request
 import pickle
 import json
 from datetime import date, datetime
+
+data = dict()
+
+label_encoder = pickle.load(open('/src/label_encoder.pickle', 'rb'))
+
+gbnn_model = pickle.load(open('/src/gbnn_model.pickle', 'rb'))
+
+output_map_object = open('classification_outputs.txt')
